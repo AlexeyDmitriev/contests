ab0dc20b34396555ff8b22222d79d9e6079d1f6d
Alexey Dmitriev <github@admitriev.name>
2013-07-27 14:50:46 +0400

    Removed empty E

--- 2013.07/2013.07.27 - Codeforces Round #194 (Div. 1)/TaskE.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import name.admitriev.spsl.io.Reader;
-import name.admitriev.spsl.io.OutputWriter;
-
-public class TaskE {
-    public void solve(int testNumber, Reader in, OutputWriter out) {
-    }
-}
